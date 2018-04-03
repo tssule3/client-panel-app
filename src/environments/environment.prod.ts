3d9994e4390ecba82efc95fa27104f1d1fb7113e
tssule3@gmail.com <tssule3@gmail.com>
2018-04-03 17:50:41 +0530

    2nd commit project complete

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC5FjpFWhAitSNxUYUVlkZpTHm-_N-QsDQ",
+    authDomain: "client-panel-app-bd51a.firebaseapp.com",
+    databaseURL: "https://client-panel-app-bd51a.firebaseio.com",
+    projectId: "client-panel-app-bd51a",
+    storageBucket: "client-panel-app-bd51a.appspot.com",
+    messagingSenderId: "755167819105"
+  }
+};

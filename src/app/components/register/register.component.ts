1a7d5f5f897f98d3019ca1103e48764c28de23ea
tssule3@gmail.com <tssule3@gmail.com>
2018-04-07 19:56:44 +0530

    19th commit bad register msg

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -11,7 +11,8 @@ import { Router } from '@angular/router';
 export class RegisterComponent implements OnInit {
   email: string;
   password: string;
-
+  errBlock = 0;
+  checkErrBlock = false;
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -19,6 +20,7 @@ export class RegisterComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    console.log('localStorage ' + localStorage.getItem('count'));
   }
 
   onSubmit() {
@@ -30,9 +32,21 @@ export class RegisterComponent implements OnInit {
         this.router.navigate(['/']);
       })
       .catch(err => {
-        this.flashMessage.show('Enter Valid Email', {
-          cssClass: 'alert-danger', timeout: 4000
-        });
+        if ( Number(localStorage.getItem('count')) > 3 ) {
+          this.flashMessage.show('Please Wait 10 seconds Before Registering In Again', {cssClass: 'alert-danger', timeout: 10000} );
+          this.checkErrBlock = true;
+          setTimeout(() => {
+            this.checkErrBlock = false;
+          }, 10000);
+        } else {
+          let userCount = 4 - this.errBlock;
+          this.flashMessage.show('Enter Valid Email ' + userCount + ' chances left', {
+            cssClass: 'alert-danger', timeout: 4000
+          });
+          // console.log(4 - this.errBlock + ' chances  left');
+        }
+        this.errBlock += 1;
+        localStorage.setItem('count', this.errBlock.toString());
       });
   }
 
